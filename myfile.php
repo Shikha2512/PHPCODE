823204b1a0ce44eb7a7fc3e40a79a86fdc273c9c
shikha2512 <shikha.bansal_cs17@gla.ac.in>
2020-01-13 15:40:55 +0530

    initial commit

--- myfile.php
+++ myfile.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="color: green">
+THIS IS PHP<br>
+    
+</body>
+</html>
 <?php
 
 echo "This is my first PHP code.  " ;
 print "This is second line" ;
 
 ?>
+</html>
