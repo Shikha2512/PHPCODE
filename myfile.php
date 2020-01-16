581926637b5cb9ba567346292d2bb98dc44c8cc1
shikha2512 <shikha.bansal_cs17@gla.ac.in>
2020-01-16 19:22:06 +0530

    operator form code

--- myfile.php
+++ myfile.php
@@ -16,5 +16,35 @@ THIS IS PHP<br>
 echo "This is my first PHP code.  " ;
 print "This is second line" ;
 
+#Variables are case sensitive
+$ABC=25;
+echo $ABC; 
+
+#Constant are not case sensitive
+
+define("$ABC",50);
+
+$ABC=40;
+echo "<br>";
+echo $ABC;
+#Datatypes is PHP.PHP is loosely types language.
+$var1="Shikha Bansal";
+$var2=25;
+$var3=12.0;
+$var4=true;
+$var5=null;
+$var6=array(1,2,3);
+
+#var_dump()- it gives the type of the variable and also abot the content
+
+var_dump($var1);
+echo $var2;
+echo $var3;
+echo $var4;
+echo $var5;
+var_dump($var6);
+
+
+
 ?>
 </html>
