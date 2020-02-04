fb13ef010bf878cdc6a05b992e6dfe13950aec24
shikha2512 <shikha.bansal_cs17@gla.ac.in>
2020-02-04 19:39:02 +0530

    commit

---
+++ prog2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+	//defining a variable
+	$mark = "60";
+	if($mark === 60){
+		echo "you have an A Grade";
+	}
+	else {
+		echo "you have an B Grade";	
+	}
+ // With the if statement your code only executes only 
+ // when the condition is true.   
+?>
+</body>
+</html>
