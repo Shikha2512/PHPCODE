2531fe759b6d5931dd6622362acb4fdcd83085d5
shikha2512 <shikha.bansal_cs17@gla.ac.in>
2020-02-04 19:54:28 +0530

    commit

--- prog2.php
+++ prog2.php
@@ -17,7 +17,50 @@
 		echo "you have an B Grade";	
 	}
  // With the if statement your code only executes only 
- // when the condition is true.   
+ // when the condition is true.  
+ echo "<br>";
+ $gender = 'FF';
+    if ($gender == 'F'){
+        echo "FEMALE";
+    }
+    else { 
+        echo "MALE";
+    } 
+
+    echo "<br>";
+    $marks = 55;
+    
+    if ($marks>79){
+        echo "A";
+    }
+    elseif($marks<=79 and  $marks>60) { 
+        echo "B";
+    }
+    elseif($marks<=60 && $marks>50) { 
+        echo "C";
+    }
+    elseif($marks=50) { 
+        echo "D";
+    }
+    else{
+        echo "F";
+    }
+
+    echo "<br>";
+$number1 = 40;
+$number2 = 12;
+
+if ($number1 != $number2) {
+    echo 'number1 is different from number2';
+    echo '<br>';
+    if ($number1 > $number2) {
+        echo 'number1 is greater than number2';
+    } else {
+        echo 'number2 is greater than number1';
+    }
+} else {
+    echo 'number1 is equal to number2';
+}
 ?>
 </body>
 </html>
